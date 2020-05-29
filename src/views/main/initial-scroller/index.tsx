17f3973e20a5aafab844528c24e362015eff823f
emsknock <emsknock@users.noreply.github.com>
2020-05-29 14:05:44 +0300

    Move scroll listener to effect hook

--- src/views/main/initial-scroller/index.tsx
+++ src/views/main/initial-scroller/index.tsx
@@ -8,17 +8,6 @@ export const InitialScroller: FC = () => {
 
     const initials = "abcdefghijklmnopqrstuvwxyzåäö".split("");
 
-    const onScroll = () => {
-        const { round, min, max } = Math;
-        const scrollLeft = listRef.current?.scrollLeft ?? 0;
-        // The 32 is based on the letters being 2rem wide with a 16px base font.
-        // This must be adjusted if the styling changes.
-        const idx = round(scrollLeft / 48);
-        // Makes sure the idx is in bounds for the alphabet (e.g. no negative numbers)
-        // since rubber banding is a thing
-        const boundIdx = max(0, min(idx, initials.length - 1));
-        setSelectedIdx(boundIdx);
-    }
     const onClick = (idx: number) => () => {
         listRef.current?.scrollTo({ behavior: "smooth", left: idx * 48 });
     }
@@ -26,9 +15,24 @@ export const InitialScroller: FC = () => {
     useEffect(
         () => {
             if (listRef.current) {
-                listRef.current.addEventListener("scroll", onScroll, { passive: true });
+                listRef.current.addEventListener(
+                    "scroll",
+                    () => {
+                        const { round, min, max } = Math;
+                        const scrollLeft = listRef.current?.scrollLeft ?? 0;
+                        // The 32 is based on the letters being 2rem wide with a 16px base font.
+                        // This must be adjusted if the styling changes.
+                        const idx = round(scrollLeft / 48);
+                        // Makes sure the idx is in bounds for the alphabet (e.g. no negative numbers)
+                        // since rubber banding is a thing
+                        const boundIdx = max(0, min(idx, initials.length - 1));
+                        setSelectedIdx(boundIdx);
+                    },
+                    { passive: true }
+                );
             }
         },
+        // eslint-disable-next-line
         [listRef]
     );
 
@@ -39,6 +43,7 @@ export const InitialScroller: FC = () => {
                 initials.map(
                     (letter, idx) => <div
                         className={`initials__letter ${idx === selectedIdx ? "--sel" : ""}`}
+                        key={letter}
                         onClick={onClick(idx)}
                     >
                         {letter}
